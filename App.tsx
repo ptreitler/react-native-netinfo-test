dddb94b6aa545b34f132194bebb6d714a2223390
Peter Treitler <peter.treiter@axtesys.at>
2023-04-07 11:19:57 +0200

    display NetInfo

--- App.tsx
+++ App.tsx
@@ -1,10 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import NetInfo, { NetInfoState } from "@react-native-community/netinfo";
+import { useState } from "react";
 
 export default function App() {
+  const [connectionInfo, setConnectionInfo] = useState<
+    NetInfoState | undefined
+  >(undefined);
+
+  NetInfo.addEventListener((info) => {
+    console.log("Received NetInfo:", info);
+    if (info !== connectionInfo) setConnectionInfo(info);
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>isConnected:</Text>
+      <Text>{`${connectionInfo?.isConnected}`}</Text>
+      <Text>isInternetReachable:</Text>
+      <Text>{`${connectionInfo?.isInternetReachable}`}</Text>
+      <Text>type:</Text>
+      <Text>{`${connectionInfo?.type}`}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +29,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
